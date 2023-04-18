17757e1b9962956678897e8e5b072c16bcfc2160
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-04-18 21:06:46 +0700

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Pham Nguyen Phu on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
