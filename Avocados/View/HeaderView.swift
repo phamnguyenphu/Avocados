d76f3edd48ab950db42d7314048957f325c20e2e
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-04-20 13:56:50 +0700

    Design the layout of the header in Xcode

---
+++ Avocados/View/HeaderView.swift
@@ -0,0 +1,68 @@
+//
+//  HeaderView.swift
+//  Avocados
+//
+//  Created by Pham Nguyen Phu on 20/04/2023.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    // MARK: - PROPERTY
+    
+    @State private var showHeadline: Bool = false
+    
+    var slideInAnimation: Animation {
+        Animation.spring(response: 1.5, dampingFraction: 0.5, blendDuration: 0.5)
+            .speed(1)
+            .delay(0.25)
+    }
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ZStack {
+            Image("avocado-slice-1")
+                .resizable()
+                .scaledToFill()
+            HStack(spacing: 0) {
+                Rectangle()
+                    .fill(Color("ColorGreenLight"))
+                    .frame(width: 5)
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Avocado")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .shadow(radius: 3)
+                    
+                    Text("Avocados are a powerhouse ingredient at any meal for anyone.")
+                        .font(.footnote)
+                        .lineLimit(2)
+                        .foregroundColor(.white)
+                        .shadow(radius: 3)
+                } //: VStack
+                .padding(.horizontal, 20)
+                .frame(width: 281, height: 105)
+                .background(
+                    Color("ColorBlackTransparentLight")
+                )
+            } //: HStack
+            .frame(height: 105)
+            .offset(x: -66, y: showHeadline ? 75 : 220)
+            .animation(slideInAnimation, value: showHeadline)
+            .onAppear {
+                showHeadline.toggle()
+            }
+        } //: ZStack
+        .frame(width: 480, height: 320, alignment: .center)
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
